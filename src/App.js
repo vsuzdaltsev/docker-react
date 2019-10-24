b671fcaf41cdb3d9296dd2c082107baaee82ce16
vsuzdaltsev <suzdaltsev_v@mail.ru>
2019-10-24 15:56:00 +0300

    change main page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! From App.js! changed
+          Hi there! From App.js! changed!
         </p>
         <a
           className="App-link"
